d714d761443cff878cb5d186f57c3dd2fb2a2f2c
Camila <camila.collangelli@gmail.com>
2026-06-03 17:53:43 -0300

    Sending Text

---
+++ sending_text.lua
@@ -0,0 +1,13 @@
+-- 7 = lowest severity / 0 = highest severity
+-- uses '..' to concatenate with some string or numerical value
+
+
+local loop_counter = 0
+
+function simple_loop()
+    gcs:send_text(7, "loop_counter = " .. loop_counter)
+    loop_counter = loop_counter + 1
+    return simple_loop, 1000
+end
+
+return simple_loop()
